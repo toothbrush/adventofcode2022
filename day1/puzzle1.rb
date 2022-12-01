490402c2b7343987596bfe5630f5087b20db4eee
paul <423357+toothbrush@users.noreply.github.com>
2022-12-01 18:26:54 -0500

    Accept Rubocop's autoformat

--- day1/puzzle1.rb
+++ day1/puzzle1.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
-def max(a,b)
-  a>b ? a : b
+def max(a, b)
+  a > b ? a : b
 end
 
 calories_this_elf = 0
@@ -9,7 +9,7 @@ calories_max = 0
 
 $stdin.read.lines.each do |l|
   this_line = l.strip
-  if this_line == ""
+  if this_line == ''
     calories_max = max(calories_max, calories_this_elf)
     calories_this_elf = 0
   else
