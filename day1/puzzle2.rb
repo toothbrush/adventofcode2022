6d0df124b0445c68f34953a64bbe3d7a7cb1f043
paul <423357+toothbrush@users.noreply.github.com>
2022-12-01 00:47:45 -0500

    Damn, min, of course!

--- day1/puzzle2.rb
+++ day1/puzzle2.rb
@@ -5,8 +5,9 @@ calories_max = [0]
 
 $stdin.read.lines.each do |l|
   this_line = l.strip
+
   if this_line == ""
-    if calories_max.max < calories_this_elf
+    if calories_max.min < calories_this_elf
       calories_max.push(calories_this_elf)
       if calories_max.length > 3
         calories_max = calories_max.sort.reverse.take(3)
