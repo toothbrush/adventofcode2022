6a8fcbc152ae3f554fac5692d57170537f668502
paul <423357+toothbrush@users.noreply.github.com>
2022-12-01 00:47:55 -0500

    Let's be correct

--- day1/puzzle2.rb
+++ day1/puzzle2.rb
@@ -20,5 +20,14 @@ $stdin.read.lines.each do |l|
   end
 end
 
-puts calories_max
+# And a fixup for if the last elf is a max-elf.
+if calories_max.min < calories_this_elf
+  calories_max.push(calories_this_elf)
+  if calories_max.length > 3
+    calories_max = calories_max.sort.reverse.take(3)
+  end
+end
+
+puts "------------------"
+pp calories_max
 puts calories_max.sum
