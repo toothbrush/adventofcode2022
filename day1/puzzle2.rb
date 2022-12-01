f442fab1953b4461b26389dd3efea8c8932da87c
paul <423357+toothbrush@users.noreply.github.com>
2022-12-01 18:32:34 -0500

    Submit to more Rubocop autoformatting

--- day1/puzzle2.rb
+++ day1/puzzle2.rb
@@ -6,12 +6,10 @@ calories_max = [0]
 $stdin.each_line do |l|
   this_line = l.strip
 
-  if this_line == ""
+  if this_line == ''
     if calories_max.min < calories_this_elf
       calories_max.push(calories_this_elf)
-      if calories_max.length > 3
-        calories_max = calories_max.sort.reverse.take(3)
-      end
+      calories_max = calories_max.sort.reverse.take(3) if calories_max.length > 3
     end
 
     calories_this_elf = 0
@@ -23,11 +21,9 @@ end
 # And a fixup for if the last elf is a max-elf.
 if calories_max.min < calories_this_elf
   calories_max.push(calories_this_elf)
-  if calories_max.length > 3
-    calories_max = calories_max.sort.reverse.take(3)
-  end
+  calories_max = calories_max.sort.reverse.take(3) if calories_max.length > 3
 end
 
-puts "------------------"
+puts '------------------'
 pp calories_max
 puts calories_max.sum
