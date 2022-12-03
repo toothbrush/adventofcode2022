969a962086bf1ce047af3f71593d8f8762899651
paul <423357+toothbrush@users.noreply.github.com>
2022-12-03 15:26:40 -0500

    day2: managed to get first part in one

--- day2/main.go
+++ day2/main.go
@@ -15,6 +15,14 @@ const (
 	Scissors
 )
 
+type PairOutcome int8
+
+const (
+	Lose PairOutcome = iota
+	Draw
+	Win
+)
+
 func (m Move) String() string {
 	switch m {
 	case Rock:
@@ -51,6 +59,65 @@ func fromResponse(c string) Move {
 	panic(fmt.Sprintf("unreasonable response provided: %s", c))
 }
 
+func (c Move) losesTo(r Move) PairOutcome {
+	switch c {
+	case Rock:
+		switch r {
+		case Rock:
+			return Draw
+		case Paper:
+			return Win
+		case Scissors:
+			return Lose
+		}
+	case Paper:
+		switch r {
+		case Rock:
+			return Lose
+		case Paper:
+			return Draw
+		case Scissors:
+			return Win
+		}
+	case Scissors:
+		switch r {
+		case Rock:
+			return Win
+		case Paper:
+			return Lose
+		case Scissors:
+			return Draw
+		}
+	}
+	panic("impossible")
+}
+
+func pointsForRound(challenge Move, response Move) (mypoints int64) {
+	mypoints = 0
+
+	// did i win?
+	switch challenge.losesTo(response) {
+	case Win:
+		mypoints += 6
+	case Draw:
+		mypoints += 3
+	case Lose:
+		mypoints += 0
+	}
+
+	// add points for the response i used
+	switch response {
+	case Rock:
+		mypoints += 1
+	case Paper:
+		mypoints += 2
+	case Scissors:
+		mypoints += 3
+	}
+
+	return mypoints
+}
+
 func main() {
 	s := bufio.NewScanner(os.Stdin)
 
@@ -58,6 +125,9 @@ func main() {
 	var line []string
 	var challenge Move
 	var response Move
+
+	var thisRoundPoints int64
+	var totalPoints int64 = 0
 
 	for s.Scan() {
 		t = s.Text()
@@ -69,5 +139,10 @@ func main() {
 		fmt.Printf("challenge: %s\n", challenge)
 		fmt.Printf("response:  %s\n", response)
 
+		thisRoundPoints = pointsForRound(challenge, response)
+		fmt.Printf("points for round = %d\n", thisRoundPoints)
+		totalPoints += thisRoundPoints
 	}
+
+	fmt.Printf("total points: %d\n", totalPoints)
 }
