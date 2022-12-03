75d13fe9d1251a4f692671a5183070ae926567f8
paul <423357+toothbrush@users.noreply.github.com>
2022-12-03 15:48:45 -0500

    Can read example input

--- day3/main.go
+++ day3/main.go
@@ -10,8 +10,21 @@ func main() {
 	s := bufio.NewScanner(os.Stdin)
 
 	var t string
+	var rucksack1 string
+	var rucksack2 string
+	var items int
+
 	for s.Scan() {
 		t = s.Text()
-		fmt.Println(t)
+
+		items = len(t)
+		if items%2 != 0 {
+			panic("non-even string length")
+		}
+
+		rucksack1 = string(t[:int(items/2)])
+		rucksack2 = string(t[int(items/2):])
+
+		fmt.Printf("%s // %s\n", rucksack1, rucksack2)
 	}
 }
