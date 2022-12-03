31b94e90836b5f18b4031581a63df3d18995953a
paul <423357+toothbrush@users.noreply.github.com>
2022-12-03 15:10:32 -0500

    Cool i can read the input file.

--- day2/main.go
+++ day2/main.go
@@ -1,7 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Move int8
+
+const (
+	Rock Move = iota
+	Paper
+	Scissors
+)
+
+func (m Move) String() string {
+	switch m {
+	case Rock:
+		return "rock"
+	case Paper:
+		return "paper"
+	case Scissors:
+		return "scissors"
+	}
+	panic("unreasonable move provided")
+}
+
+func fromChallenge(c string) Move {
+	switch c {
+	case "A":
+		return Rock
+	case "B":
+		return Paper
+	case "C":
+		return Scissors
+	}
+	panic(fmt.Sprintf("unreasonable challenge provided: %s", c))
+}
+
+func fromResponse(c string) Move {
+	switch c {
+	case "X":
+		return Rock
+	case "Y":
+		return Paper
+	case "Z":
+		return Scissors
+	}
+	panic(fmt.Sprintf("unreasonable response provided: %s", c))
+}
 
 func main() {
-	fmt.Println("foo")
+	s := bufio.NewScanner(os.Stdin)
+
+	var t string
+	var line []string
+	var challenge Move
+	var response Move
+
+	for s.Scan() {
+		t = s.Text()
+		line = strings.Split(t, " ")
+
+		challenge = fromChallenge(line[0])
+		response = fromResponse(line[1])
+
+		fmt.Printf("challenge: %s\n", challenge)
+		fmt.Printf("response:  %s\n", response)
+
+	}
 }
