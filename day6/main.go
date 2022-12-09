f0a22ee2a5e581fcc19009a8d487c9315ee3eb8e
paul <423357+toothbrush@users.noreply.github.com>
2022-12-08 21:20:38 -0500

    Okay that was easy

--- day6/main.go
+++ day6/main.go
@@ -19,8 +19,11 @@ func all_different(ring []rune) bool {
 		counts[c]++
 	}
 	// fmt.Printf("%v\n", counts)
-	return len(counts) == 4
+	return len(counts) == MESSAGE_LENGTH
 }
+
+// for puzzle 1 it's 4, for puzzle 2 it's 14
+const MESSAGE_LENGTH = 14
 
 func run() (err error) {
 	s := bufio.NewScanner(os.Stdin)
@@ -29,12 +32,12 @@ func run() (err error) {
 	for s.Scan() {
 		t = s.Text()
 		fmt.Printf("processing: %s...\n", t)
-		ring := make([]rune, 4)
+		ring := make([]rune, MESSAGE_LENGTH)
 		for i, c := range t {
 			// poorman's ring buffer
-			ring[i%4] = c
+			ring[i%MESSAGE_LENGTH] = c
 			// no point checking if we haven't seen enough characters yet
-			if i >= 4 {
+			if i >= MESSAGE_LENGTH {
 				if all_different(ring) {
 					fmt.Printf("Packet found at %d.\n", i+1)
 					break
