6daa452481d0c951a3a7e1f6a3a51021bee32884
paul <423357+toothbrush@users.noreply.github.com>
2022-12-09 21:12:28 -0500

    okay, basic parsing

--- day7/main.go
+++ day7/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,14 +14,56 @@ func main() {
 	}
 }
 
+type Command struct {
+	cmd    string
+	args   []string
+	output []string
+}
+
+func NewCommand(cmdLine string) (Command, error) {
+	split := strings.Split(cmdLine, " ")
+	if split[0] != "$" {
+		return Command{}, fmt.Errorf("oh scheisse command doesn't start with $!")
+	}
+	command := Command{}
+	command.cmd = split[1]
+	for _, arg := range split[2:] {
+		command.args = append(command.args, arg)
+	}
+	command.output = make([]string, 0)
+	return command, nil
+}
+
 func run() (err error) {
 	s := bufio.NewScanner(os.Stdin)
 	var t string
 
+	var commands []Command
+	commands = make([]Command, 0)
+
 	for s.Scan() {
 		t = s.Text()
-		fmt.Printf("executing: %s...\n", t)
+		if strings.HasPrefix(t, "$") {
+			// oh this is a command being executed
+			// let's make a new slot for it
+			fmt.Printf("executing: `%s`\n", t)
+			command, err := NewCommand(t)
+			if err != nil {
+				return err
+			}
+			commands = append(commands, command)
+		} else {
+			// it's the output of a command
+			// by definition we're working on the last command added to the list
+			if len(strings.TrimSpace(t)) > 0 { // skip blank lines
+				fmt.Printf("output: [%s]\n", t)
+				last := &commands[len(commands)-1]
+				last.output = append(last.output, t)
+			}
+		}
+
 	}
 
+	fmt.Printf("%v\n", commands)
 	return nil
 }
