31bad7e9ea05155ecf28233825b2cb9d0a600504
paul <423357+toothbrush@users.noreply.github.com>
2022-12-11 18:20:47 -0500

    awful but it works

--- day8/main.go
+++ day8/main.go
@@ -90,6 +90,51 @@ func (directions Directions) anyVisible() bool {
 	return res
 }
 
+func NewDirections() Directions {
+	directions := Directions{
+		{dx: 0, dy: 1, is_visible: true},
+		{dx: 0, dy: -1, is_visible: true},
+		{dx: 1, dy: 0, is_visible: true},
+		{dx: -1, dy: 0, is_visible: true},
+	}
+	return directions
+}
+
+func (g *Grid) determineVisibility() {
+	for i := range g.treez {
+		for j := range g.treez[i] {
+			// from the pov of tree i,j walk till we're at the horizon.
+			// set up a bunch of directions, start by assuming the tree is visible in that direction:
+			directions := NewDirections()
+			// look for occlusions:
+			for d_i, dir := range directions {
+				// figure out if we're walking horizontally or vertically
+				if dir.dy == 0 {
+					for x := i; x >= 0 && x < g.size; x += dir.dx {
+						if g.treez[x][j].height >= g.treez[i][j].height {
+							// occluded from this direction!
+							if x != i {
+								directions[d_i].is_visible = false
+							}
+						}
+					}
+				} else {
+					for y := j; y >= 0 && y < g.size; y += dir.dy {
+						if g.treez[i][y].height >= g.treez[i][j].height {
+							// occluded from this direction!
+							// small fixup - don't compare with myself
+							if y != j {
+								directions[d_i].is_visible = false
+							}
+						}
+					}
+				}
+			}
+			g.treez[i][j].is_visible = directions.anyVisible()
+		}
+	}
+}
+
 func (g Grid) countVisible() int {
 	total := 0
 	for i := range g.treez {
@@ -118,6 +163,7 @@ func run() (err error) {
 	}
 	g := NewGrid(len(lines))
 	g.populateGrid(lines)
+	g.determineVisibility()
 
 	fmt.Printf("%v\n", g)
 	fmt.Printf("visible: %d\n", g.countVisible())
