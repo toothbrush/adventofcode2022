9f56d9855316370a1c93c19b73e876adb472dfac
paul <423357+toothbrush@users.noreply.github.com>
2022-12-11 22:25:11 -0500

    very rudimentary and wrong

--- day10/main.go
+++ day10/main.go
@@ -5,12 +5,41 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func main() {
-	if err := run(); err != nil {
-		log.Fatal(err)
+type State struct {
+	X     int
+	cycle int
+}
+
+type Instruction struct {
+	name string
+	args []string
+}
+
+func (s *State) execute(i Instruction) error {
+	switch i.name {
+	case "noop":
+		s.cycle++
+	case "addx":
+		arg1, err := strconv.Atoi(i.args[0])
+		if err != nil {
+			return err
+		}
+		s.X += arg1
+		s.cycle++
+		s.cycle++
 	}
+	fmt.Printf("Executing `%s`. State = %v\n", i.name, s)
+	return nil
+}
+
+func NewState() State {
+	s := State{}
+	s.X = 1
+	return s
 }
 
 func run() (err error) {
@@ -19,11 +48,23 @@ func run() (err error) {
 	s := bufio.NewScanner(os.Stdin)
 	var t string
 
+	state := NewState()
 	for s.Scan() {
 		t = s.Text()
 		if len(t) > 0 {
-			fmt.Println(t)
+			spl := strings.Split(t, " ")
+			i := Instruction{name: spl[0], args: spl[1:]}
+			err = state.execute(i)
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
 }
