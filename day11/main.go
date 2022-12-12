458e7626b9a571bbf2819711f048746577966f10
paul <423357+toothbrush@users.noreply.github.com>
2022-12-11 23:49:34 -0500

    sigh here we go

--- day11/main.go
+++ day11/main.go
@@ -5,7 +5,54 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
 )
+
+type ParseState int
+
+const (
+	MonkeyIntro ParseState = iota
+	Items
+	Test
+	IfTrue
+	IfFalse
+	MonkeyComplete
+)
+
+type Monkey struct {
+	id int
+}
+
+func parseMonkeyList(input []string) ([]Monkey, error) {
+	state := MonkeyIntro
+	var m Monkey
+	ms := []Monkey{}
+	for _, i := range input {
+		fmt.Printf("[%d] parsing: `%s`\n", state, i)
+		switch state {
+		case MonkeyIntro:
+			m = Monkey{}
+			intro := regexp.MustCompile("Monkey ([0-9]+):")
+			id_string := intro.FindStringSubmatch(i)[1]
+			id, err := strconv.Atoi(id_string)
+			if err != nil {
+				return []Monkey{}, err
+			}
+			m.id = id
+			state = MonkeyComplete
+		case Items:
+		case Test:
+		case IfTrue:
+		case IfFalse:
+		case MonkeyComplete:
+			ms = append(ms, m)
+			state = MonkeyIntro
+		}
+	}
+
+	return ms, nil
+}
 
 func run() (err error) {
 	fmt.Printf("welcome to monkeys\n")
@@ -13,12 +60,19 @@ func run() (err error) {
 	s := bufio.NewScanner(os.Stdin)
 	var t string
 
+	input := make([]string, 0)
 	for s.Scan() {
 		t = s.Text()
 		if len(t) > 0 {
 			fmt.Printf("%s\n", t)
+			input = append(input, t)
 		}
 	}
+	monkeys, err := parseMonkeyList(input)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%v\n", monkeys)
 	return nil
 }
 
